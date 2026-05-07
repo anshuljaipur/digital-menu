93ac659d4d253d50efe43e680c3eb9bedeada2e9
anshuljaipur <122553325+anshuljaipur@users.noreply.github.com>
2026-05-07 14:29:07 +0530

    Add files via upload

---
+++ database.js
@@ -0,0 +1,43 @@
+// Add your PANs with a 'category' field
+const masterData = [
+    { pan: "APKPG5866P", name: "ANSHUL GOYAL", category: "GOYAL", isTax: "Yes" },
+    { pan: "BYUPG2812F", name: "ANKITA GOYAL", category: "GOYAL", isTax: "No" },
+    { pan: "AASPG5102D", name: "VIJAY GOYAL", category: "GOYAL", isTax: "No" },
+    { pan: "AASPG5015F", name: "GOPAL KRISHAN GOYAL", category: "GOYAL", isTax: "No" },
+
+    { pan: "AAVPW3870B", name: "MANEW WATTS", category: "CHARU", isTax: "No" },
+    { pan: "ACYPW3232E", name: "CHARU WATTS", category: "CHARU", isTax: "Yes" },
+    { pan: "AAJPW1017E", name: "NEELAM WATTS", category: "CHARU", isTax: "No" },
+    { pan: "AALHN5098L", name: "NEELAM WATTS HUF", category: "CHARU", isTax: "No" },
+    { pan: "AAFHC4341H", name: "CHARU WATTS HUF", category: "CHARU", isTax: "No" },
+
+    { pan: "AKPPB2980H", name: "Mradul Bhutra", category: "MRADUL", isTax: "Yes" },
+    { pan: "BRFPS7922G", name: "Rashmi Bhutra", category: "MRADUL", isTax: "Yes" },
+    { pan: "AEFPB4915E", name: "Manju Bhutra", category: "MRADUL", isTax: "No" },
+    { pan: "AFAPB3879D", name: "Rampyari Bhutra", category: "MRADUL", isTax: "No" },
+    { pan: "AAVHM6302C", name: "Mradul Bhutra HUF", category: "MRADUL", isTax: "No" },
+
+    { pan: "ALUPG5390A", name: "ANKIT GUPTA", category: "ANKIT", isTax: "No" },
+    { pan: "DOFPS3569C", name: "ITI GUPTA", category: "ANKIT", isTax: "No" },
+    { pan: "BWZPG0074N", name: "AMAIRA GUPTA", category: "ANKIT", isTax: "No" },
+    { pan: "AAQHA4141Q", name: "ANKIT GUPTA HUF", category: "ANKIT", isTax: "No" },
+    { pan: "BYCPG0803J", name: "PREETI GUPTA", category: "ANKIT", isTax: "No" },
+    { pan: "BYKPG5975R", name: "KRITI GUPTA", category: "ANKIT", isTax: "No" },
+    { pan: "EYSPG4201D", name: "SHIVIKA GUPTA", category: "ANKIT", isTax: "No" },
+    { pan: "AMZPG9698P", name: "ARPIT GUPTA", category: "ANKIT", isTax: "Yes" },
+    { pan: "AMZPG9699N", name: "GOURI SHANKAR GUPTA", category: "ANKIT", isTax: "Yes" },
+    { pan: "ABOPG3434H", name: "SUNITA GUPTA", category: "ANKIT", isTax: "No" },
+    { pan: "AMZPG9700E", name: "ASHISH GUPTA", category: "ANKIT", isTax: "Yes" },
+
+    { pan: "AERPB1169G", name: "ANIL BHUTRA", category: "ANIL", isTax: "No" },
+    { pan: "ACAPB1920A", name: "USHA DEVI", category: "ANIL", isTax: "No" },
+    { pan: "ACLPM1517F", name: "SEEMA", category: "ANIL", isTax: "No" },
+    { pan: "AAIHA2049D", name: "ANIL HUF", category: "ANIL", isTax: "No" },
+    { pan: "AAOHS8432A", name: "SSB HUF", category: "ANIL", isTax: "No" },
+    { pan: "GDTPB1568J", name: "ARYAN BHUTRA", category: "ANIL", isTax: "No" },
+    { pan: "CKFPB4721C", name: "NANDITA BHUTRA", category: "ANIL", isTax: "No" },
+
+    { pan: "ACGPB4333H", name: "AMITA BINDAL", category: "BINDAL", isTax: "No" },
+    { pan: "ABYPA0641E", name: "NAWAL KISHORE AGARWAL", category: "BINDAL", isTax: "No" },
+    { pan: "AADHN7866A", name: "NAWAL KISHORE HUF", category: "BINDAL", isTax: "No" },
+];
